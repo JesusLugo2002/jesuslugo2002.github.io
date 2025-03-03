b2f2aaf3fd4f70c7c15e3a1cf586fc8920354191
JesusLugo2002 <jesusdlugo02@gmail.com>
2025-03-03 01:29:53 +0100

    Fix on about section

--- src/components/About.vue
+++ src/components/About.vue
@@ -21,10 +21,15 @@ function copyEmail() {
     <div class="container-fluid bg-dark py-5">
         <div class="container">
             <div class="row">
-                <p class="h2 text-white col-6">Hi! I'm <b>Jesús Lugo</b>, a videogame developer, and I create, develop, design and write.</p>
-                <p class="text-white h5 pt-4">If you have any questions or just want to say hi, feel free to send me a message. :)</p>
-                <p class="text-white" v-if="!emailCopied">>> <span class="text-decoration-underline" @click="copyEmail">{{ email }}</span></p>
-                <p class="text-white" v-else>>> Email copied!</p>
+                <div class="col-sm-6">
+                    <p class="text-white h2">Hi! I'm <b>Jesús Lugo</b>, a videogame developer, and I create, develop, design and write.</p>
+                    <p class="text-white h5 pt-4">If you have any questions or just want to say hi, feel free to send me a message. :)</p>
+                    <p class="text-white" v-if="!emailCopied">>> <span class="text-decoration-underline" @click="copyEmail">{{ email }}</span></p>
+                    <p class="text-white" v-else>>> Email copied!</p>
+                </div>
+                <div class="col-sm-6">
+                    <p class="text-white">Image</p>
+                </div>
             </div>
         </div>
     </div>
