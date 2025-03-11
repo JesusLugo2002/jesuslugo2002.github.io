d1101a826b910c9fccd6a1d46792634f81424b7c
JesusLugo2002 <jesusdlugo02@gmail.com>
2025-03-11 09:56:19 +0100

    Progress with Skills

--- src/components/MySkills.vue
+++ src/components/MySkills.vue
@@ -2,25 +2,16 @@
     <div id="skills-container" class="container my-4">
         <div class="row">
             <div class="col-12 px-5 py-4 d-flex flex-column col-lg-4">
-                <div class="placeholder align-self-center mb-4">
-                    <!-- Aqui irá el logo dibujado en pixel-art -->
-                </div>
                 <h3>As videogame lover...</h3>
                 <p>I consider video games to be complete cultural works, which can captivate us with rich experiences of different art forms. I have enjoyed them since I was a child and I am committed to creating my own stories to captivate more people.</p>
                 <p><span>Favourites videogames: </span>Red Dead Redemption 2, Ultrakill, Bloodborne, Hollow Knight and Cult of the Lamb.</p>
             </div>
             <div class="col-12 px-5 py-4 d-flex flex-column col-lg-4">
-                <div class="placeholder align-self-center mb-4">
-                    <!-- Aqui irá el logo dibujado en pixel-art -->
-                </div>
                 <h3>As developer...</h3>
                 <p>Organization, simplicity, and clean code are the most important things to me, so it's something I try to improve. I like learning to solve patterns in a simple way, finding efficient solutions, and reinforcing principles and good practices in my knowledge.</p>
                 <p><span>Dev tools: </span>Visual Studio Code, Vue, Django, Bootstrap, Godot Engine, Pixi.js</p>
             </div>
             <div class="col-12 px-5 py-4 d-flex flex-column col-lg-4">
-                <div class="placeholder align-self-center mb-4">
-                    <!-- Aqui irá el logo dibujado en pixel-art -->
-                </div>
                 <h3>As artist (well, future artist)...</h3>
                 <p>I have a clear tendency towards pixel-art and every day I take a step closer to perfecting this beautiful and complex technique. As in any creative discipline, the process of improvement is continuous, and there is always a long way to go.</p>
                 <p><span>Fun fact: </span>all the pixel-art you find on this page was drawn by me c:</p>
--- src/components/WorkItem.vue
+++ src/components/WorkItem.vue
@@ -4,21 +4,21 @@ const { work } = defineProps(['work'])
 
 <template>
     <a class="col-12 p-0 col-md-4 d-flex flex-column" :href="work.url" target="_blank">
-        <div class="w-100 image"></div>
+        <img :src="work.cover" :alt="work.title + ' cover'" class="img-fluid w-100">
         <h3 class="flex-grow-1">{{ work.title }}</h3>
     </a>
 </template>
 
 <style scoped lang="scss">
 a {
-    color: $white;
-    .image {
+    color: $dark_1;
+    img {
         height: 200px;
-        background-color: $white;
+        background-image: none;
     }
     h3 {
         font-family: $pixel-font;
-        background-color: $gray;
+        background-color: $light_gray;
     }
 }
 </style>
