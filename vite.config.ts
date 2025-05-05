a5aeb7307376c7f80a5e0ae92bb934b1b4fc149a
JesusLugo2002 <jesusdlugo02@gmail.com>
2025-05-05 12:48:58 +0200

    Modified file

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,6 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/jesuslugo2002.github.io/',
   plugins: [
     vue(),
     vueDevTools(),
